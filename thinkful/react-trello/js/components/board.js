7e8f94fe5110161e271a57ae63df5abe7d76d1dc
johnvincentio <john@johnvincent.io>
2017-09-01 17:46:58 -0400

    add

--- thinkful/react-trello/js/components/board.js
+++ thinkful/react-trello/js/components/board.js
@@ -8,12 +8,24 @@ export default class Board extends React.Component {
         super(props);
     }
 
+    handleSubmit(event) {
+        console.log('(board) A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    onAddInputChanged(event) {
+        console.log("(board) onAddInputChanged");
+        this.setState({value: event.target.value});
+    }
+
     render() {
         const jv = this.props.lists.map((item, idx) => {
             return (
                 <div key={item.id}>
                     <h3 className="each board">Board title: {item.title}</h3>
-                    <List title={item.list.title} cards={item.list.cards}/>
+                    <List title={item.list.title} cards={item.list.cards} 
+                        onChange={this.onAddInputChanged}
+                        onSubmit={this.handleSubmit}/>
                 </div>
             )
         });
--- thinkful/react-trello/js/components/list.js
+++ thinkful/react-trello/js/components/list.js
@@ -6,7 +6,23 @@ import Card from './card';
 export default class List extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {value: ''};
+        this.handleChange = this.props.onChange.bind(this);
+        this.handleSubmit = this.props.onSubmit.bind(this);
+
+        // this.handleChange = this.handleChange.bind(this);
+        // this.handleSubmit = this.handleSubmit.bind(this);
     }
+
+    // handleSubmit(event) {
+    //     alert('A name was submitted: ' + this.state.value);
+    //     event.preventDefault();
+    // }
+
+    // handleChange(event) {
+    //     console.log("handlechange");
+    //     this.setState({value: event.target.value});
+    // }
 
     render() {
         const jv = this.props.cards.map((item, idx) => {
@@ -19,10 +35,18 @@ export default class List extends React.Component {
         return (
             <div>
                 {jv}
+                <form id="js--submit" onSubmit={this.handleSubmit}>
+                    <div className="js--error-msg form-error"></div>
+                    <div>
+                        <input id="card" name="card" type="text" required placeholder="Card" 
+                                value={this.state.value} onChange={this.handleChange} />
+                    </div>
+
+                    <button className="submit-button" type="submit">Add Card</button>
+                </form>
             </div>
         );
     }
-
 
 }
 
