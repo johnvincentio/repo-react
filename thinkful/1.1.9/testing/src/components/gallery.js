51e728ff270f1cd28f254f8d5c2e6ae3ab2e85b8
johnvincentio <john@johnvincent.io>
2017-10-05 12:21:21 -0400

    react

--- thinkful/1.1.9/testing/src/components/gallery.js
+++ thinkful/1.1.9/testing/src/components/gallery.js
@@ -5,7 +5,7 @@ import Image from './image';
 
 export default function Gallery(props) {
   const images = props.images.map((image, index) =>
-    <Image url={image.url} description={image.description} key={index} />
+    <Image url={image.url} description={image.description} key={image.id} />
   );
 
   return (
