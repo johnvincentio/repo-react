633652467ff458b3761292b945763a79fd650801
johnvincentio <john@johnvincent.io>
2017-10-07 12:21:55 -0400

    clean

--- thinkful/OLD/thinkful-react-starter/js/index.js
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import ReactDOM  from 'react-dom';
-
-import PersonList from './components/person-list';
-
-document.addEventListener('DOMContentLoaded', () =>
-    ReactDOM.render(<PersonList />, document.getElementById('app'))
-);
