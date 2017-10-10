e3b5419f05d451a585ea033f1baec9ca084909ec
johnvincentio <john@johnvincent.io>
2017-10-10 10:53:55 -0400

    2.1.2

--- thinkful/2.1.2/hot-cold/src/reducers/index.js
+++ thinkful/2.1.2/hot-cold/src/reducers/index.js
@@ -1,24 +1,18 @@
 
 import * as actions from '../actions/index';
 
-const initialRepositoryState = [{ guessed: [] }];
+const initialRepositoryState = { guessed: [] };
 
 // eslint-disable-next-line no-mixed-operators
 const randomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-export const repositoryReducer = (state = initialRepositoryState, action) => {
+const repositoryReducer = (state = initialRepositoryState, action) => {
   if (action.type === actions.USER_GUESSED_NUMBER) {
-    console.log(state);
-    console.log(state[0]);
-    console.log(state[0].guessed);
-    state[0].guessed.push(action.guess);
-    return state;
+    return Object.assign({}, state, { guessed: [action.guess, ...state.guessed] });
   }
 
   if (action.type === actions.RANDOM_NUMBER) {
-    return [...state, {
-      random: randomInteger(action.fromNumber, action.toNumber),
-    }];
+    return Object.assign({}, state, { random: randomInteger(action.fromNumber, action.toNumber) });
   }
 
   return state;
