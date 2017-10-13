416df48d7301ea4708b98ed8a1611cc3f869aa98
johnvincentio <john@johnvincent.io>
2017-10-13 09:29:58 -0400

    2.1.4

---
+++ thinkful/2.1.4/store/src/index.js
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import store from './store';
+import RepositoryList from './components/repository-list';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+      <Provider store={store}>
+          <RepositoryList />
+      </Provider>,
+      document.getElementById('root'),
+  );
+});
+
+/*
+import * as actions from './actions/index';
+import store from './store';
+
+store.dispatch(actions.addRepository('joe'));
+console.log(store.getState()); // Logs [{ name: 'joe', rating: null}]
+*/
