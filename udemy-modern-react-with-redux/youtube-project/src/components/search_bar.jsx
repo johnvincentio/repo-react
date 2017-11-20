50a879f6d3d3b4d02258013ed58206538252a56a
johnvincentio <john@johnvincent.io>
2017-11-20 13:53:52 -0500

    add

--- udemy-modern-react-with-redux/youtube-project/src/components/search_bar.jsx
+++ udemy-modern-react-with-redux/youtube-project/src/components/search_bar.jsx
@@ -1,8 +1,20 @@
 
 import React from 'react';
 
-const SearchBar = () => (
-	<input />
-);
+export class SearchBar extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleNewGame = this.props.onNewGame.bind(this);
+		this.handleHelp = this.handleHelp.bind(this);
+	}
+
+	onInputChange() {
+
+	}
+
+	render() {
+		return <input onChange={this.onInputChange} />;
+	}
+}
 
 export default SearchBar;
