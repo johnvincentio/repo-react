a3475998276d7f8a386a1bc0a4035fd602daab69
johnvincentio <john@johnvincent.io>
2017-11-22 16:59:50 -0500

    add

--- udemy-modern-react-with-redux/youtube-project/src/components/search_bar.jsx
+++ udemy-modern-react-with-redux/youtube-project/src/components/search_bar.jsx
@@ -4,16 +4,26 @@ import React from 'react';
 export class SearchBar extends React.Component {
 	constructor(props) {
 		super(props);
-		this.handleNewGame = this.props.onNewGame.bind(this);
-		this.handleHelp = this.handleHelp.bind(this);
+		this.state = { term: '' };
+		// this.handleNewGame = this.props.onNewGame.bind(this);
+		// this.handleHelp = this.handleHelp.bind(this);
 	}
 
-	onInputChange() {
-
-	}
+	// onInputChange(event) {
+	// 	console.log(event.target.value);
+	// }
+	// return <input onChange={this.onInputChange} />
 
 	render() {
-		return <input onChange={this.onInputChange} />;
+		return (
+			<div>
+				<input
+					value={this.state.term}
+					onChange={event => this.setState({ term: event.target.value })}
+				/>
+				Value of the input {this.state.term}
+			</div>
+		);
 	}
 }
 
