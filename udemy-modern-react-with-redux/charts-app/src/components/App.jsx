7e8537513bb0a1afd550a3449e4fa3ff1ba25611
johnvincentio <john@johnvincent.io>
2017-12-05 17:49:46 -0500

    app

---
+++ udemy-modern-react-with-redux/charts-app/src/components/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+import SearchBar from '../containers/search_bar';
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
+}
---
+++ udemy-modern-react-with-redux/charts-app/src/reducers/index.js
@@ -0,0 +1,9 @@
+
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+	// books: BooksReducer,
+	// activeBook: ActiveBook,
+});
+
+export default rootReducer;
