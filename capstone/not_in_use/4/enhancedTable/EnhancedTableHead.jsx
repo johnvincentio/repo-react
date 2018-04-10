eed50bcb8559d2c987a1ba0b80f523a03f18fc98
johnvincentio <john@johnvincent.io>
2018-04-10 18:21:43 -0400

    more

---
+++ capstone/not_in_use/4/enhancedTable/EnhancedTableHead.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { TableCell, TableHead, TableRow, TableSortLabel } from 'material-ui/Table';
+import Checkbox from 'material-ui/Checkbox';
+import Tooltip from 'material-ui/Tooltip';
+
+import { tableHeadersType } from '../../../../types';
+
+class EnhancedTableHead extends React.Component {
+	createSortHandler = property => event => {
+		this.props.onRequestSort(event, property);
+	};
+
+	render() {
+		const { onSelectAllClick, order, orderBy, numSelected, rowCount, tableHeaders } = this.props;
+
+		return (
+			<TableHead>
+				<TableRow>
+					<TableCell padding="checkbox">
+						<Checkbox
+							indeterminate={numSelected > 0 && numSelected < rowCount}
+							checked={numSelected === rowCount}
+							onChange={onSelectAllClick}
+						/>
+					</TableCell>
+					{tableHeaders.map(
+						column => (
+							<TableCell
+								key={column.id}
+								numeric={column.numeric}
+								padding={column.disablePadding ? 'none' : 'default'}
+								sortDirection={orderBy === column.id ? order : false}
+							>
+								<Tooltip
+									title="Sort"
+									placement={column.numeric ? 'bottom-end' : 'bottom-start'}
+									enterDelay={300}
+								>
+									<TableSortLabel
+										active={orderBy === column.id}
+										direction={order}
+										onClick={this.createSortHandler(column.id)}
+									>
+										{column.label}
+									</TableSortLabel>
+								</Tooltip>
+							</TableCell>
+						),
+						this
+					)}
+				</TableRow>
+			</TableHead>
+		);
+	}
+}
+
+EnhancedTableHead.propTypes = {
+	numSelected: PropTypes.number.isRequired,
+	onRequestSort: PropTypes.func.isRequired,
+	onSelectAllClick: PropTypes.func.isRequired,
+	order: PropTypes.string.isRequired,
+	orderBy: PropTypes.string.isRequired,
+	rowCount: PropTypes.number.isRequired,
+	tableHeaders: tableHeadersType.isRequired
+};
+
+export default EnhancedTableHead;
