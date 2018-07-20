2ba30badf8d2b3d7c256765d25be772508f56e3f
johnvincentio <john@johnvincent.io>
2018-07-20 16:58:41 -0400

    1.1.4

---
+++ thinkful/1.1.4/project/src/components/list.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import Card from './card';
+
+export default class List extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			cards: [
+				{
+					text: 'Example card 1'
+				},
+				{
+					text: 'Example card 2'
+				},
+				{
+					text: 'Example card 3'
+				}
+			]
+		};
+	}
+
+	render() {
+		const cards = this.state.cards.map((card, index) => (
+			<li key={index}>
+				<Card {...card} />
+			</li>
+		));
+		return (
+			<div>
+				<h3>{this.props.title}</h3>
+				<ul className="list">{cards}</ul>
+			</div>
+		);
+	}
+}
+
+List.defaultProps = {
+	title: ''
+};
---
+++ thinkful/1.1.4/react-trello-props-state/README.md
@@ -0,0 +1,7 @@
+# React Trello app with props and state
+
+[GitHub](https://github.com/Thinkful-Ed/react-trello-props-state)
+
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+
+
