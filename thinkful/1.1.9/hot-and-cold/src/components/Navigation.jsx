3814862ed2d09450602370ead38152a45958ba6a
johnvincentio <john@johnvincent.io>
2018-07-24 13:52:42 -0400

    1.1.9

--- thinkful/1.1.9/hot-and-cold/src/components/Navigation.jsx
+++ thinkful/1.1.9/hot-and-cold/src/components/Navigation.jsx
@@ -3,28 +3,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import styled from 'styled-components';
+
+export const Nav = styled.nav`
+	position: relative;
+	height: 10%;
+	padding: 1em;
+`;
+
+export const Li = styled.li`
+	text-transform: uppercase;
+	font-weight: 700;
+	font-size: 1.2em;
+
+	&:first-child {
+		float: left;
+	}
+	&:last-child {
+		float: right;
+	}
+`;
+
+export const Button = styled.button`
+	color: #fff;
+	cursor: pointer;
+	border: 0;
+	margin: 0;
+	padding: 0;
+	background: #1f253d;
+	font-size: 1.2em;
+`;
+
+export const Title = styled.h1`
+	font-weight: 900;
+	font-size: 3em;
+	padding: 0.8em;
+	color: #fff;
+`;
+
 export default function Navigation(props) {
-	console.log('Game');
+	console.log('Navigation 99');
 	return (
 		<header>
-			<nav>
+			<Nav>
 				<ul className="clearfix">
-					<li>
-						<button onClick={props.toggleHelp}>What!</button>
-					</li>
-					<li>
-						<a className="what" href="#">
-							What ?
-						</a>
-					</li>
-					<li>
-						<a className="new" href="#">
-							+ New Game
-						</a>
-					</li>
+					<Li>
+						<Button onClick={props.toggleHelp}>What?</Button>
+					</Li>
+					<Li>
+						<Button onClick={props.toggleHelp}>+ New Game</Button>
+					</Li>
 				</ul>
-			</nav>
-			<h1>HOT or COLD</h1>
+			</Nav>
+			<Title>HOT or COLD</Title>
 		</header>
 	);
 }
