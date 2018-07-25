944ee5f41edba81a1c90c1c25193dfb168f0d2cd
johnvincentio <john@johnvincent.io>
2018-07-25 10:25:34 -0400

    1.1.9

---
+++ thinkful/1.1.9/hot-and-cold-1/src/components/Navigation.jsx
@@ -0,0 +1,64 @@
+//
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styled from 'styled-components';
+
+export const Nav = styled.nav`
+	position: relative;
+	height: 10%;
+	padding: 1em;
+`;
+
+export const Li = styled.li`
+	&:first-child {
+		float: left;
+	}
+	&:last-child {
+		float: right;
+	}
+`;
+
+export const Button = styled.button`
+	color: #fff;
+	cursor: pointer;
+	border: 0;
+	margin: 0;
+	padding: 0;
+	background: #1f253d;
+
+	text-transform: uppercase;
+	font-weight: 700;
+	font-size: 1.2em;
+`;
+
+export const Title = styled.h1`
+	font-weight: 900;
+	font-size: 3em;
+	padding: 0.8em;
+	color: #fff;
+`;
+
+export default function Navigation(props) {
+	return (
+		<header>
+			<Nav>
+				<ul className="clearfix">
+					<Li>
+						<Button onClick={props.toggleHelp}>What?</Button>
+					</Li>
+					<Li>
+						<Button onClick={props.toggleGame}>+ New Game</Button>
+					</Li>
+				</ul>
+			</Nav>
+			<Title>HOT or COLD</Title>
+		</header>
+	);
+}
+
+Navigation.propTypes = {
+	toggleHelp: PropTypes.func.isRequired,
+	toggleGame: PropTypes.func.isRequired
+};
