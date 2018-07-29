6b99db8b7b12ff9d7779f4ef4befd4018d5245e5
johnvincentio <john@johnvincent.io>
2018-07-29 14:27:09 -0400

    jest

--- thinkful/1.3.2/hot-and-cold/tests/__tests__/components/Help.jsx
+++ thinkful/1.3.2/hot-and-cold/tests/__tests__/components/Help.jsx
@@ -3,49 +3,69 @@
 /* global describe, it, jest, expect */
 
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 import { Help } from '../../../src/components';
 
 describe('<Help />', () => {
 	const toggleHelp = jest.fn();
 
-	it('Renders without crashing', () => {
-		shallow(<Help toggleHelp={toggleHelp} />);
-	});
-	it('Renders classname help', () => {
-		const wrapper = shallow(<Help toggleHelp={toggleHelp} />);
-		expect(wrapper.hasClass('help')).toEqual(true);
-	});
-	it('Renders h3', () => {
-		const wrapper = shallow(<Help toggleHelp={toggleHelp} />);
-		expect(wrapper.find('h3').text()).toEqual('What do I do?');
-	});
-	it('Renders h3 contains', () => {
-		const wrapper = shallow(<Help toggleHelp={toggleHelp} />);
-		expect(wrapper.contains(<h3>What do I do?</h3>)).toBeTruthy();
+	describe('basic', () => {
+		it('Renders without crashing', () => {
+			shallow(<Help toggleHelp={toggleHelp} />);
+		});
 	});
 
-	it('Renders contains ul', () => {
-		const wrapper = shallow(<Help toggleHelp={toggleHelp} />);
-		expect(
-			wrapper
-				.find('div')
-				.find('div')
-				.find('p')
-		).toHaveLength(2);
-	});
-	it('Renders contains ul', () => {
-		const wrapper = shallow(<Help toggleHelp={toggleHelp} />);
-		expect(wrapper.find('p')).toHaveLength(2);
+	describe('content', () => {
+		it('Renders classname help', () => {
+			const wrapper = shallow(<Help toggleHelp={toggleHelp} />);
+			expect(wrapper.hasClass('help')).toEqual(true);
+		});
+		it('Renders h3', () => {
+			const wrapper = shallow(<Help toggleHelp={toggleHelp} />);
+			expect(wrapper.find('h3').text()).toEqual('What do I do?');
+		});
+		it('Renders h3 contains', () => {
+			const wrapper = shallow(<Help toggleHelp={toggleHelp} />);
+			expect(wrapper.contains(<h3>What do I do?</h3>)).toBeTruthy();
+		});
+
+		it('Renders contains ul', () => {
+			const wrapper = shallow(<Help toggleHelp={toggleHelp} />);
+			expect(
+				wrapper
+					.find('div')
+					.find('div')
+					.find('p')
+			).toHaveLength(2);
+		});
+		it('Renders contains ul', () => {
+			const wrapper = shallow(<Help toggleHelp={toggleHelp} />);
+			expect(wrapper.find('p')).toHaveLength(2);
+		});
+
+		it('Renders contains ul', () => {
+			const wrapper = shallow(<Help toggleHelp={toggleHelp} />);
+			expect(wrapper.find('ul')).toHaveLength(1);
+		});
+		it('Renders contains li', () => {
+			const wrapper = shallow(<Help toggleHelp={toggleHelp} />);
+			expect(wrapper.find('li')).toHaveLength(3);
+		});
 	});
 
-	it('Renders contains ul', () => {
-		const wrapper = shallow(<Help toggleHelp={toggleHelp} />);
-		expect(wrapper.find('ul')).toHaveLength(1);
-	});
-	it('Renders contains li', () => {
-		const wrapper = shallow(<Help toggleHelp={toggleHelp} />);
-		expect(wrapper.find('li')).toHaveLength(3);
+	describe.only('button', () => {
+		it('Find the button', () => {
+			const wrapper = shallow(<Help toggleHelp={toggleHelp} />);
+			expect(wrapper.find('button').text()).toEqual('Got It!');
+		});
+		it.only('Fire the button', () => {
+			const callback = jest.fn();
+			const wrapper = mount(<Help toggleHelp={callback} />);
+			expect(callback).not.toHaveBeenCalled();
+
+			wrapper.find('button').simulate('click');
+			expect(callback).toHaveBeenCalled();
+		});
 	});
 });
