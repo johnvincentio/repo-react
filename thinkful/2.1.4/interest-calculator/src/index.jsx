dd8f34615adfda6609fb0bac516e1ea84ac94ccf
johnvincentio <john@johnvincent.io>
2018-08-02 13:53:39 -0400

    2.1.4

---
+++ thinkful/2.1.4/interest-calculator/src/index.jsx
@@ -0,0 +1,8 @@
+//
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import InterestCalculator from './components/InterestCalculator';
+import './index.css';
+
+ReactDOM.render(<InterestCalculator />, document.getElementById('root'));
