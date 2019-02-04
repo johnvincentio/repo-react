840d50c02e451f3fa4a22b3c42202c04eea74160
johnvincentio <john@johnvincent.io>
2019-02-04 16:18:01 -0500

    add

---
+++ udemy-modern-react-with-redux/myapps/190/client/src/components/Modal.jsx
@@ -0,0 +1,25 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import history from '../history';
+
+const Modal = props => {
+	return ReactDOM.createPortal(
+		<div
+			onClick={() => history.push('/')}
+			className="ui dimmer modals visible active"
+		>
+			<div onClick={(e) => e.stopPropagation()} className="ui standard modal visible active">
+				<div className="header">Delete Stream</div>
+				<div className="content">Are you sure you want to dekete this stream?</div>
+				<div className="actions">
+					<div className="ui primary button">Delete</div>
+					<div className="ui cancel button">Cancel</div>
+				</div>
+			</div>
+		</div>,
+		document.querySelector('#modal')
+	);
+}
+
+export default Modal;
