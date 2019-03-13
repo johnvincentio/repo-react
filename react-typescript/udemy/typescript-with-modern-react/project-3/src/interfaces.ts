5a473b797f4bf82438245e28ac9ddf4d8a44dee6
johnvincentio <john@johnvincent.io>
2019-03-13 12:40:23 -0400

    add

--- react-typescript/udemy/typescript-with-modern-react/project-3/src/interfaces.ts
+++ react-typescript/udemy/typescript-with-modern-react/project-3/src/interfaces.ts
@@ -1,5 +1,7 @@
 //
 
+export type Dispatch = React.Dispatch<IAction>;
+
 export interface IState {
 	episodes: Array<IEpisode>;
 	favourites: Array<IEpisode>;
@@ -7,7 +9,7 @@ export interface IState {
 
 export interface IAction {
 	type: string;
-	payload: any;
+	payload: Array<IEpisode> | any;
 }
 
 export interface IEpisode {
@@ -26,7 +28,7 @@ export interface IEpisode {
 
 export interface IEpisodeProps {
 	episodes: Array<IEpisode>;
-	store: { state: IState; dispatch: any };
-	toggleFavAction: (state: IState, dispatch: any, episode: IEpisode) => IAction;
+	store: { state: IState; dispatch: Dispatch };
+	toggleFavAction: (state: IState, dispatch: Dispatch, episode: IEpisode) => IAction;
 	favourites: Array<IEpisode>;
 }
