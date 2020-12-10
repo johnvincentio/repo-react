990748ee729a8e46b709d31777ac506099e6c95f
johnvincentio <john@johnvincent.io>
2020-12-10 17:58:56 -0500

    add

--- hooks/hooks-context-api/my-code/context-project/src-orig/serviceWorker.js
+++ hooks/hooks-context-api/my-code/context-project/src-orig/serviceWorker.js
@@ -10,31 +10,38 @@
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read https://bit.ly/CRA-PWA
 
+/* global window, navigator, fetch */
+
+/* eslint-disable no-param-reassign */
+
 const isLocalhost = Boolean(
 	window.location.hostname === 'localhost' ||
-    // [::1] is the IPv6 localhost address.
-    window.location.hostname === '[::1]' ||
-    // 127.0.0.1/8 is considered localhost for IPv4.
-    window.location.hostname.match(
-    	/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
+		// [::1] is the IPv6 localhost address.
+		window.location.hostname === '[::1]' ||
+		// 127.0.0.1/8 is considered localhost for IPv4.
+		window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
 );
 
 export function register(config) {
+	// console.log('serviceWorker::register - stage 1');
 	if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+		// console.log('serviceWorker::register - stage 2');
 		// The URL constructor is available in all browsers that support SW.
-		const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+		const publicUrl = new URL(process.env.HOME_URL, window.location.href);
 		if (publicUrl.origin !== window.location.origin) {
-			// Our service worker won't work if PUBLIC_URL is on a different origin
+			// console.log('serviceWorker::register - stage 3');
+			// Our service worker won't work if HOME_URL is on a different origin
 			// from what our page is served on. This might happen if a CDN is used to
 			// serve assets; see https://github.com/facebook/create-react-app/issues/2374
 			return;
 		}
-
+		// console.log('serviceWorker::register - stage 4');
 		window.addEventListener('load', () => {
-			const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+			// console.log('serviceWorker::register - stage 5');
+			const swUrl = `${process.env.HOME_URL}/service-worker.js`;
 
 			if (isLocalhost) {
+				// console.log('serviceWorker::register - stage 6');
 				// This is running on localhost. Let's check if a service worker still exists or not.
 				checkValidServiceWorker(swUrl, config);
 
@@ -43,11 +50,12 @@ export function register(config) {
 				navigator.serviceWorker.ready.then(() => {
 					console.log(
 						'This web app is being served cache-first by a service ' +
-              'worker. To learn more, visit https://bit.ly/CRA-PWA'
+							'worker. To learn more, visit https://bit.ly/CRA-PWA'
 					);
 				});
 			} else {
 				// Is not localhost. Just register service worker
+				// console.log('serviceWorker::register - stage 7');
 				registerValidSW(swUrl, config);
 			}
 		});
@@ -71,7 +79,7 @@ function registerValidSW(swUrl, config) {
 							// content until all client tabs are closed.
 							console.log(
 								'New content is available and will be used when all ' +
-                  'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
+									'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
 							);
 
 							// Execute callback
@@ -104,10 +112,7 @@ function checkValidServiceWorker(swUrl, config) {
 		.then(response => {
 			// Ensure service worker exists, and that we really are getting a JS file.
 			const contentType = response.headers.get('content-type');
-			if (
-				response.status === 404 ||
-        (contentType != null && contentType.indexOf('javascript') === -1)
-			) {
+			if (response.status === 404 || (contentType != null && contentType.indexOf('javascript') === -1)) {
 				// No service worker found. Probably a different app. Reload the page.
 				navigator.serviceWorker.ready.then(registration => {
 					registration.unregister().then(() => {
@@ -120,9 +125,7 @@ function checkValidServiceWorker(swUrl, config) {
 			}
 		})
 		.catch(() => {
-			console.log(
-				'No internet connection found. App is running in offline mode.'
-			);
+			console.log('No internet connection found. App is running in offline mode.');
 		});
 }
 
--- hooks/hooks-context-api/my-code/context-project/src/index.jsx
+++ hooks/hooks-context-api/my-code/context-project/src/index.jsx
@@ -1,98 +1,12 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import { MuiThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
+ReactDOM.render(<App />, document.getElementById('root'));
 
-import { appTheme } from './themes/themes';
-
-import Root from './root/Root';
-// import App from './components/App';
-
-import { register } from './serviceWorker';
-
-import configureStore from './store/configureStore';
-
-// import { authUser } from './redux/actions/user.actions';
-
-const store = configureStore();
-
-// console.log(`NODE_ENV ${process.env.NODE_ENV}`);
-
-/*
-const token = localStorage.getItem('token');
-if (token) {
-	// console.log('*** found a token');
-	store.dispatch(authUser(token));
-}
-*/
-
-
-document.addEventListener('DOMContentLoaded', () => {
-	ReactDOM.render(
-		<MuiThemeProvider theme={appTheme}>
-			<CssBaseline />
-			<Provider store={store}>
-				<Root />
-			</Provider>
-		</MuiThemeProvider>,
-		document.getElementById('root')
-	);
-});
-
-
-register();
-
-/*
-ReactDOM.render(
-	<App />,
-	document.getElementById('root')
-);
-*/
-
-/*
-document.addEventListener('DOMContentLoaded', () => {
-	ReactDOM.render(
-		<App />,
-		document.getElementById('root')
-	);
-});
-*/
-
-/*
-document.addEventListener('DOMContentLoaded', () => {
-	ReactDOM.render(
-		<Router>
-			<Root store={store} />
-		</Router>,
-		document.getElementById('root'),
-	);
-});
-*/
-
-/*
-document.addEventListener('DOMContentLoaded', () => {
-	ReactDOM.render(
-		<Provider store={store}>
-			<Root />
-		</Provider>,
-		document.getElementById('root')
-	);
-});
-*/
-
-/*
-document.addEventListener('DOMContentLoaded', () => {
-	ReactDOM.render(
-		<Provider store={store}>
-			<App />
-			<div className="devtools">
-				<DevTools />
-			</div>
-		</Provider>,
-		document.getElementById('root')
-	);
-});
-*/
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
