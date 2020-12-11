54891a87ad1941f104306b2bb82fa6bfd029f39d
johnvincentio <john@johnvincent.io>
2020-12-10 23:08:09 -0500

    add

--- hooks/hooks-context-api/my-code/hooks-project/src/App.jsx
+++ hooks/hooks-context-api/my-code/hooks-project/src/App.jsx
@@ -1,20 +1,22 @@
 
 import React from 'react';
+
 import Navbar from './Navbar';
 import Form from './Form';
 import PageContent from './PageContent';
+
 import { ThemeProvider } from './contexts/ThemeContext';
 import { LanguageProvider } from './contexts/LanguageContext';
 
-const App = () => (
-	<ThemeProvider>
-		<LanguageProvider>
-			<PageContent>
-				<Navbar />
-				<Form />
-			</PageContent>
-		</LanguageProvider>
-	</ThemeProvider>
-);
-
-export default App;
+export default function App() {
+	return (
+		<ThemeProvider>
+			<LanguageProvider>
+				<PageContent>
+					<Navbar />
+					<Form />
+				</PageContent>
+			</LanguageProvider>
+		</ThemeProvider>
+	);
+}
--- hooks/hooks-context-api/my-code/hooks-project/src/contexts/ThemeContext.jsx
+++ hooks/hooks-context-api/my-code/hooks-project/src/contexts/ThemeContext.jsx
@@ -1,24 +1,16 @@
 /* eslint-disable react/prefer-stateless-function */
 
-import React, { Component, createContext } from 'react';
+import React, { createContext } from 'react';
+
+import useToggleState from '../hooks/useToggleState';
 
 export const ThemeContext = createContext();
 
-export class ThemeProvider extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { isDarkMode: false };
-	}
-
-	toggleTheme = () => {
-		this.setState({ isDarkMode: !this.state.isDarkMode });
-	}
-
-	render() {
-		return (
-			<ThemeContext.Provider value={{ ...this.state, toggleTheme: this.toggleTheme }}>
-				{this.props.children}
-			</ThemeContext.Provider>
-		);
-	}
+export function ThemeProvider(props) {
+	const [isDarkMode, toggleTheme] = useToggleState(false);
+	return (
+		<ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
+			{props.children}
+		</ThemeContext.Provider>
+	);
 }
