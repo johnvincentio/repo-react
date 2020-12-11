2bd5ed29511e40b1781267b941241cc26bfe8b81
johnvincentio <john@johnvincent.io>
2020-12-10 20:16:35 -0500

    add

---
+++ hooks/hooks-context-api/my-code/context-project/src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import Navbar from './Navbar';
+import Form from './Form';
+import PageContent from './PageContent';
+
+const App = () => (
+	<PageContent>
+		<Navbar />
+		<Form />
+	</PageContent>
+);
+
+export default App;
