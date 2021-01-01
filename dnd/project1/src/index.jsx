02c304d0b1835382544c3c1d3ab70c578bd1ac1f
johnvincentio <john@johnvincent.io>
2021-01-01 13:26:30 -0500

    add

--- dnd/project1/src/index.jsx
+++ dnd/project1/src/index.jsx
@@ -7,13 +7,18 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 
 import { appTheme } from './themes/themes';
 
-import App1 from './2/App';
+import App2 from './2/App';
+import App3 from './3/App';
+import App4 from './4/App';
+import App5 from './5/App';
+import App6 from './6/App';
+
 
 document.addEventListener('DOMContentLoaded', () => {
 	ReactDOM.render(
 		<MuiThemeProvider theme={appTheme}>
 			<CssBaseline />
-			<App1 />
+			<App6 />
 		</MuiThemeProvider>,
 		document.getElementById('root')
 	);
