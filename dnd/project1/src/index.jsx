4c14aed795e7b4e7bd43b81f2cbfd43e3b7e7671
johnvincentio <john@johnvincent.io>
2021-01-02 01:39:02 -0500

    add

--- dnd/project1/src/index.jsx
+++ dnd/project1/src/index.jsx
@@ -12,13 +12,13 @@ import App3 from './3/App';
 import App4 from './4/App';
 import App5 from './5/App';
 import App6 from './6/App';
-
+import App7 from './7/App';
 
 document.addEventListener('DOMContentLoaded', () => {
 	ReactDOM.render(
 		<MuiThemeProvider theme={appTheme}>
 			<CssBaseline />
-			<App6 />
+			<App7 />
 		</MuiThemeProvider>,
 		document.getElementById('root')
 	);
