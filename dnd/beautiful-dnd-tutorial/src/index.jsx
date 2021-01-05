7ad49fd83ca8fcf4c37cb5b27759828c25012241
johnvincentio <john@johnvincent.io>
2021-01-04 20:28:53 -0500

    add

---
+++ dnd/beautiful-dnd-tutorial/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import App from './table-column-reorder-dnd-example/App';
+// import App from './gargroh-v7-drag-column-beautiful-react-dnd/App';
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('root')
+);
+
