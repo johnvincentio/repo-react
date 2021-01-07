46b88c0795501a3435fcdec2ea0be702ae749772
johnvincentio <john@johnvincent.io>
2021-01-07 15:07:55 -0500

    add

---
+++ dnd/grid-data-project/working/work5/src/DraggableItem.jsx
@@ -0,0 +1,70 @@
+
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/prefer-stateless-function */
+
+import React from 'react';
+import styled from 'styled-components';
+import { Draggable } from 'react-beautiful-dnd';
+
+const Container = styled.div`
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+	// padding: 8px;
+	// margin-top: 6px;
+	padding: 6px 0;
+  margin-bottom: 8px;
+	background-color: ${props => (props.isDragging ? 'lightGreen' : 'white')};
+	display: flex;
+`;
+
+const Handle = styled.div`
+  width: 20px;
+  height: 20px;
+  background-color: orange;
+  border-radius: 4px;
+  margin-right: 8px;
+`;
+
+const Item = styled.div`
+border: 1px solid lightblue;
+border-radius: 2px;
+width: ${props => props.width};
+`;
+
+const obj = {
+	content: '200px',
+	status: '100px',
+	estimate: '80px'
+};
+
+export default class DraggableItem extends React.Component {
+
+	render() {
+		const { headers, row, index } = this.props;
+		return (
+			<Draggable draggableId={row.id} index={index}>
+				{(provided, snapshot) => {
+					console.log('DraggableItem; provided ', provided, 'snapshot ', snapshot);
+					return (
+						<Container
+							ref={provided.innerRef}
+							{...provided.draggableProps}
+							isDragging={snapshot.isDragging}
+						>
+							<Handle
+								{...provided.dragHandleProps}
+							/>
+							{headers.map(header => {
+								console.log('header ', header);
+								return (
+									<Item key={`item-${header.id}-${row.id}`} width={header.width}>
+										{row[header.field]}
+									</Item>
+								);
+							})}
+						</Container>
+					);}}
+			</Draggable>
+		);
+	}
+}
---
+++ dnd/grid-data-project/working/work5/src/initial-data.js
@@ -0,0 +1,26 @@
+
+const initialData = {
+	list: [
+		{ id: 'list-1', content: '1 - Take out the garbage', status: 'Active', estimate: '3w' },
+		{ id: 'list-2', content: '2 - Watch my favorite show', status: 'Started', estimate: '1d' },
+		{ id: 'list-3', content: '3 - Charge my phone', status: 'Hold', estimate: '2.5h' },
+		{ id: 'list-4', content: '4 - Cook dinner', status: 'Finished', estimate: '2w 1d' }
+	],
+	headers: [
+		{ id: 0, width: '200px', title: 'Task', field: 'content' },
+		{ id: 1, width: '100px', title: 'Status', field: 'status' },
+		{ id: 2, width: '80px', title: 'Estimate', field: 'estimate' }
+	]
+};
+
+export default initialData;
+
+// columns: {
+// 	'column-1': {
+// 		id: 'column-1',
+// 		title: 'To do',
+// 		taskIds: ['task-1', 'task-2', 'task-3', 'task-4']
+// 	}
+// },
+// Facilitate reordering of the columns
+// columnOrder: ['column-1']
