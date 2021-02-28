97e4a1cf6c3cd0ee3457d622e126c111062750c1
johnvincentio <john@johnvincent.io>
2021-02-28 17:12:03 -0500

    add

--- dnd/react-dnd/dnd1/src/App.jsx
+++ dnd/react-dnd/dnd1/src/App.jsx
@@ -1,143 +1,14 @@
+
 import React, { useEffect, useRef, useState } from 'react';
 import { DndProvider, useDrag, useDrop } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { TouchBackend } from 'react-dnd-touch-backend';
 
+import Column from './Column';
+import MovableItem from './MovableItem';
+
 import { COLUMN_NAMES } from './constants';
 import { tasks } from './tasks';
-
-const MovableItem = ({ name, index, currentColumnName, moveCardHandler, setItems }) => {
-	const changeItemColumn = (currentItem, columnName) => {
-		setItems((prevState) => prevState.map(e => ({
-			...e,
-			column: e.name === currentItem.name ? columnName : e.column
-		})));
-	};
-
-	const ref = useRef(null);
-
-	const [, drop] = useDrop({
-		accept: 'Our first type',
-		hover(item, monitor) {
-			if (!ref.current) {
-				return;
-			}
-			const dragIndex = item.index;
-			const hoverIndex = index;
-			// Don't replace items with themselves
-			if (dragIndex === hoverIndex) {
-				return;
-			}
-			// Determine rectangle on screen
-			const hoverBoundingRect = ref.current?.getBoundingClientRect();
-			// Get vertical middle
-			const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-			// Determine mouse position
-			const clientOffset = monitor.getClientOffset();
-			// Get pixels to the top
-			const hoverClientY = clientOffset.y - hoverBoundingRect.top;
-			// Only perform the move when the mouse has crossed half of the items height
-			// When dragging downwards, only move when the cursor is below 50%
-			// When dragging upwards, only move when the cursor is above 50%
-			// Dragging downwards
-			if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-				return;
-			}
-			// Dragging upwards
-			if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
-				return;
-			}
-			// Time to actually perform the action
-			moveCardHandler(dragIndex, hoverIndex);
-			// Note: we're mutating the monitor item here!
-			// Generally it's better to avoid mutations,
-			// but it's good here for the sake of performance
-			// to avoid expensive index searches.
-			item.index = hoverIndex;
-		}
-	});
-
-	const [{ isDragging }, drag] = useDrag({
-		item: { index, name, currentColumnName, type: 'Our first type' },
-		end: (item, monitor) => {
-			const dropResult = monitor.getDropResult();
-
-			if (dropResult) {
-				const { name } = dropResult;
-				const { DO_IT, IN_PROGRESS, AWAITING_REVIEW, DONE } = COLUMN_NAMES;
-				switch (name) {
-					case IN_PROGRESS:
-						changeItemColumn(item, IN_PROGRESS);
-						break;
-					case AWAITING_REVIEW:
-						changeItemColumn(item, AWAITING_REVIEW);
-						break;
-					case DONE:
-						changeItemColumn(item, DONE);
-						break;
-					case DO_IT:
-						changeItemColumn(item, DO_IT);
-						break;
-					default:
-						break;
-				}
-			}
-		},
-		collect: (monitor) => ({
-			isDragging: monitor.isDragging()
-		})
-	});
-
-	const opacity = isDragging ? 0.4 : 1;
-
-	drag(drop(ref));
-
-	return (
-		<div ref={ref} className='movable-item' style={{ opacity }}>
-			{name}
-		</div>
-	);
-};
-
-const Column = ({ children, className, title }) => {
-	const [{ isOver, canDrop }, drop] = useDrop({
-		accept: 'Our first type',
-		drop: () => ({ name: title }),
-		collect: (monitor) => ({
-			isOver: monitor.isOver(),
-			canDrop: monitor.canDrop()
-		}),
-		// Override monitor.canDrop() function
-		canDrop: (item) => {
-			const { DO_IT, IN_PROGRESS, AWAITING_REVIEW, DONE } = COLUMN_NAMES;
-			const { currentColumnName } = item;
-			return (currentColumnName === title) ||
-                (currentColumnName === DO_IT && title === IN_PROGRESS) ||
-                (currentColumnName === IN_PROGRESS && (title === DO_IT || title === AWAITING_REVIEW)) ||
-                (currentColumnName === AWAITING_REVIEW && (title === IN_PROGRESS || title === DONE)) ||
-                (currentColumnName === DONE && (title === AWAITING_REVIEW));
-		}
-	});
-
-	const getBackgroundColor = () => {
-		if (isOver) {
-			if (canDrop) {
-				return 'rgb(188,251,255)';
-			} if (!canDrop) {
-				return 'rgb(255,188,188)';
-			}
-		} else {
-			return '';
-		}
-	};
-
-	return (
-		<div ref={drop} className={className} style={{ backgroundColor: getBackgroundColor() }}>
-			<p>{title}</p>
-			{children}
-		</div>
-	);
-};
 
 const App = () => {
 	const [items, setItems] = useState(tasks);
