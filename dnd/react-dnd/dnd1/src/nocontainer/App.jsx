a15ec3204dc48316cb54f7a738a908813c71e014
johnvincentio <john@johnvincent.io>
2021-03-01 20:56:38 -0500

    add

---
+++ dnd/react-dnd/dnd1/src/nocontainer/App.jsx
@@ -0,0 +1,64 @@
+
+import React, { useState } from 'react';
+
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { TouchBackend } from 'react-dnd-touch-backend';
+
+import MovableItem from './MovableItem';
+
+import { columns } from './columns';
+
+import './app.scss';
+
+const App = () => {
+	const [items, setItems] = useState(columns);
+	const isMobile = window.innerWidth < 600;
+
+	const handleMoveList = (fromIndex, toIndex, list) => {
+		const before = toIndex < fromIndex;
+		const newList = [];
+		list.forEach((item, idx) => {
+			if (idx !== fromIndex) {
+				if (before && idx === toIndex) {
+					newList.push(list[fromIndex]);
+				}
+				newList.push(list[idx]);
+				if (!before && idx === toIndex) {
+					newList.push(list[fromIndex]);
+				}
+			}
+		});
+		return newList;
+	};
+
+	const dropHandler = (obj) => {
+		console.log('App::dropHandler; obj ', obj);
+		const { from, to } = obj;
+		const list = handleMoveList(from, to, items);
+		console.log(' items ', items, ' list ', list);
+		setItems(list);
+	};
+
+	const returnItemsForContainer = () => items.map((item, index) => (
+		<MovableItem
+			key={item.id}
+			name={item.name}
+			index={index}
+			dropHandler={dropHandler}
+		/>
+	));
+
+	return (
+		<div>
+			<DndProvider backend={isMobile ? TouchBackend : HTML5Backend}>
+				<div className='container'>
+					<p>Outer</p>
+					{returnItemsForContainer()}
+				</div>
+			</DndProvider>
+		</div>
+	);
+};
+
+export default App;
---
+++ dnd/react-dnd/dnd1/src/nocontainer/MovableItem.jsx
@@ -0,0 +1,65 @@
+
+import React, { useRef } from 'react';
+import PropTypes from 'prop-types';
+
+import { useDrag, useDrop } from 'react-dnd';
+
+const MovableItem = ({ name, index, dropHandler }) => {
+
+	const ref = useRef(null);
+
+	const [{ isOver, canDrop }, dropRef] = useDrop({
+		accept: 'column_type',
+		collect: (monitor) => ({
+			isOver: !!monitor.isOver(),
+			canDrop: monitor.canDrop()
+		}),
+		drop: (item, monitor) => {
+			console.log('MovableItem::drop; item ', item, ' monitor ', monitor);
+			const abc = monitor.getItem();
+			console.log('abc ', abc);
+			dropHandler({ from: abc.index, to: index });
+		}
+		// canDrop: (item) => true
+	});
+
+	const [{ isDragging }, dragRef] = useDrag({
+		item: { index, name, type: 'column_type' },
+		end: (item, monitor) => {
+			console.log('MovableItem::useDrag::end; item ', item, ' monitor ', monitor);
+			const dropResult = monitor.getDropResult();
+			console.log('MovableItem::useDrag; dropResult ', dropResult);
+		},
+		collect: (monitor) => ({
+			isDragging: monitor.isDragging()
+		})
+	});
+
+	const getBackgroundColor = () => {
+		if (isOver) {
+			if (canDrop) {
+				return 'rgb(188,251,255)';
+			}
+			return 'rgb(255,188,188)';
+		}
+		return '';
+	};
+
+	const opacity = isDragging ? 0.4 : 1;
+
+	dragRef(dropRef(ref));
+
+	return (
+		<div ref={ref} className='movable-item' style={{ opacity, backgroundColor: getBackgroundColor() }}>
+			{name}
+		</div>
+	);
+};
+
+MovableItem.propTypes = {
+	name: PropTypes.string.isRequired,
+	index: PropTypes.number.isRequired,
+	dropHandler: PropTypes.func.isRequired
+};
+
+export default MovableItem;
